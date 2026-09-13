78dfed93321d7f559f77e85518e14c5258a52361
sawsanwehbi <80157303+sawsanwehbi@users.noreply.github.com>
2026-09-13 10:21:22 -0700

    Update Scatter_Plots.R

--- Scatter_Plots.R
+++ Scatter_Plots.R
@@ -112,9 +112,7 @@ NoDivLUCA_NSA_plot <- ggplot(AA_properties_plots, aes(y = as.numeric(LUCA_usage)
   xlab('Number of non-H in side chain') + ylab('LUCA clan usage')  + 
   theme(legend.position="none") + scale_x_continuous(breaks = c(0,2,4,6,8,10)) + 
   geom_text(color='blue', size = 12,  position = pd) +
-  #annotate(geom="text", y=0.83, x=1, label="Class II", color="#619CFF", size = 12) +
-  #annotate(geom="text", y=0.78, x=1, label="Class I", color="#F8766D", size = 12) +
-  #annotate(geom="text", y=1.1, x=180, label="Class I & II", color="#00BA38", size = 12) +
+  annotate(geom="text", x=0, y=1.23, label="a)", color="black", size = 16) +
   geom_errorbar(aes(ymin=as.numeric(LUCA_usage)-LUCA_usageSE, ymax=as.numeric(LUCA_usage)+LUCA_usageSE), position = pd) +
   theme(axis.text=element_text(size=24),axis.title=element_text(size=32,face="bold"),
         legend.text=element_text(size=14), legend.title = element_text(size=16,face="bold")) + 
@@ -157,11 +155,7 @@ ModifiedNoDivLUCA_NSA_plot <- ggplot(BiosynReqmodified, aes(y = as.numeric(LUCA_
   geom_errorbar(aes(ymin=as.numeric(LUCA_usage)-LUCA_usageSE, ymax=as.numeric(LUCA_usage)+LUCA_usageSE), position = pd) +
   theme(axis.text=element_text(size=24),axis.title=element_text(size=32,face="bold"),
         legend.text=element_text(size=14), legend.title = element_text(size=16,face="bold")) + 
-  #geom_line(aes(y = predict(NoDivNSA_model)), linewidth = 1,color = 'black') +
-  #annotate(geom="text", x=4, y=0.73, label=paste0(paste0('Weighted R',supsc('2')), "= 0.69"), color= "black", size = 14) +
-  #annotate(geom="text", x=4, y=0.67, label="p = 5e-6", color= "black", size = 14) +
-  #annotate(geom="text", x=70, y=1.2, label="b)", color="black", size = 16) +
-  #geom_ribbon(aes(ymin=NoDivNSA_confinterval$.lower, ymax=NoDivNSA_confinterval$.upper), colour=NA, alpha=0.3)
+  annotate(geom="text", x=0, y=1.23, label="b)", color="black", size = 16) +
   geom_line(data= BiosynReqmodified[which(BiosynReqmodified$aaRS_Class==1  ),] ,aes(y = predict(ModifiedNoDivNSA_modelclass1)), linewidth = 1,color = "#F8766D") +
   geom_line(data= BiosynReqmodified[which(BiosynReqmodified$aaRS_Class==2),], aes(y = predict(ModifiedNoDivNSA_modelclass2)), linewidth = 1,color = "#00BFC4") +
   annotate(geom="text", x=3, y=0.65, label=paste0(paste0('Weighted R',supsc('2')), "= 0.52"), color= "#00BFC4", size = 14) +
@@ -170,6 +164,8 @@ ModifiedNoDivLUCA_NSA_plot <- ggplot(BiosynReqmodified, aes(y = as.numeric(LUCA_
   annotate(geom="text", x=7, y=1.15, label="p = 0.002", color= "#F8766D", size = 14) +
   geom_ribbon(data=BiosynReqmodified[which(BiosynReqmodified$aaRS_Class==1  ),] , aes(ymin=ModifiedNoDivNSA_model_confintervalclass1$.lower, ymax=ModifiedNoDivNSA_model_confintervalclass1$.upper), colour="#F8766D", alpha=0.3) +
   geom_ribbon(data=BiosynReqmodified[which(BiosynReqmodified$aaRS_Class==2 ),] , aes(ymin=ModifiedNoDivNSA_model_confintervalclass2$.lower, ymax=ModifiedNoDivNSA_model_confintervalclass2$.upper), colour="#00BFC4", alpha=0.3)
+
+grid.arrange(NoDivLUCA_NSA_plot, ModifiedNoDivLUCA_NSA_plot, ncol=2)
 
 ## Supp Figure 1
 NoDivConclan_wls_model <- lm(AA_properties_plots$Trifonov_order ~ AA_properties_plots$LUCA_usage)
